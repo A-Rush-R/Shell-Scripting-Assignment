a405aa2498b96c2fa3f81a1369e9b78fdd583e35
Arush <arush.up@gmail.com>
2024-03-19 17:20:42 +0530

    Tasks completed

--- main.sh
+++ main.sh
@@ -4,6 +4,19 @@ dashes() {
     echo "------------------" >> $outfile  # Append dashes to the output file
 }
 
+# function to calculate average salary for a given city
+calculate_average() {
+    local infile="$1"
+    local outfile="$2"
+    local city="$3"
+
+    # Calculate the average of the 4th column for records matching the given city
+    # maintain count to keep track of number of records for finding the average
+    average=$(awk -F', ' -v c="$city" '$3 == c {sum += $4; count++} END {print sum/count}' "$infile")
+
+    echo "City: $city, Salary: $average" >> $outfile
+}
+
 echo Hello World
 
 if [ $# -ne 2 ] 
@@ -48,11 +61,24 @@ awk -F',' 'NR>1 {print $0}' $infile | sort -t',' -k4r | awk -F',' 'NR<4 {print}'
 dashes
 echo Details of average salary of each city: >> $outfile
 
-# Logic for avcerage salary
+# find the unique cities (removing the header)
+unique_cities=$(awk -F ', ' 'NR>1 {print $3}' $infile | sort | uniq)
+
+# Iterate over each unique city and calculate average
+while IFS= read -r city; do
+    calculate_average $infile $outfile "$city"
+done <<< "$unique_cities"
 
 dashes 
 echo Details of individuals with a salary above the overall average salary: >> $outfile
 
-# Logic for individuals with salary above overall average
+# First we find the average salary
+# Remove the ', ' as the delimiter, extract the salaries and then loop over them to find the sum
+# Then divide by number of records (NR-1, accounting for the header) to find the header
+average=$(awk -F', ' '{print $4}' $infile | awk '{sum += $1} END {print sum/(NR-1)}')
+
+# Next we filter the records where the salary is greater than average 
+# Remove ',' as the delimiter, remove the header and then print if salary is greater than average
+awk -v avg="$average" -F',' 'NR > 1 && $4 > avg {print}' $infile >> $outfile
 
 dashes
